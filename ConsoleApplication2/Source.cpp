d625c123e9e1a40ef5149dfa8a95118536c9be27
Alaa <Alaa.Abdulfattah@aun.edu.eg>
2021-10-20 09:24:55 +0200

    First commit

---
+++ ConsoleApplication2/Source.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a, b; 
+	cout << "Enter two numbers:" << endl;
+	cin >> a >> b;
+	cout << endl;
+	cout << a << " + " << b << " = " << a + b << endl;
+	system("pause");
+	return 0;
+}                   
